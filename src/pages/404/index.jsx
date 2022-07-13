fbfbdf4561db1b3324f823024c88bf700147f91e
valent1618 <valentinlemaire1618@gmail.com>
2022-07-13 17:13:48 +0200

    add variation depend of the error

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,15 +1,28 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function Error() {
+import Ghost from '../../assets/ghost';
+import NoUser from '../../assets/noUser';
+
+function Error({ error = 'page' }) {
   return (
     <main id='Error'>
+      {error === 'page' ? <Ghost /> : <NoUser />}
       <h1>
         Error <b>404</b>
       </h1>
-      <h2>Oups! La page que vous demandez n'existe pas.</h2>
-      <Link to='/SportSee/'>Retourner sur le dashboard</Link>
+      <h2>
+        {error === 'page'
+          ? `Oups! La page que vous demandez n'existe pas.`
+          : `L'utilisateur ne semble pas exister...`}
+      </h2>
+      <Link to='/SportSee/'>Retourner à l'accueil</Link>
     </main>
   );
 }
+
+Error.propTypes = {
+  error: PropTypes.oneOf(['page', 'user']),
+};
 
 export default Error;
