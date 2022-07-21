269b62e871ead7bfba450bccac3d0a96e4e36ba7
valent1618 <valentinlemaire1618@gmail.com>
2022-07-21 13:31:25 +0200

    return formatted performance

--- src/functions/Get/getUserPerformance.js
+++ src/functions/Get/getUserPerformance.js
@@ -1,20 +1,17 @@
 import axios from 'axios';
+import formatPerformance from '../Format/formatPerformance';
 
 /**
  * Get user performance
  * @param { Function } setUserPerformance from the useState hook
  * @param { String } userId
- * @return { (Object | Error) } Object {
- *  userId,
- *  kind: {1, 2, 3, 4, 5, 6},
- *  data: [{value, kind}, {value, kind}, ...]
- * }
+ * @return { (Object | Error) } Object {value, kind}
  */
 function getUserPerformance(setUserPerformance, userId) {
   axios
     .get(`http://localhost:3000/user/${userId}/performance`)
     .then((response) => {
-      setUserPerformance(response.data.data);
+      setUserPerformance(formatPerformance(response.data.data));
     })
     .catch((error) => {
       console.log('Error with getUserPerformance =>', error);
