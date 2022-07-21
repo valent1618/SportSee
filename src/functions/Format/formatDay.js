b3749f81b3c08002a573ba195a904ce33000e787
valent1618 <valentinlemaire1618@gmail.com>
2022-07-21 13:29:44 +0200

    format day to first day letter

---
+++ src/functions/Format/formatDay.js
@@ -0,0 +1,23 @@
+function formatDay(day) {
+  switch (day) {
+    case 1:
+      return 'L';
+    case 2:
+      return 'M';
+    case 3:
+      return 'M';
+    case 4:
+      return 'J';
+    case 5:
+      return 'V';
+    case 6:
+      return 'S';
+    case 7:
+      return 'D';
+    default:
+      console.log('Day is out of range => ', day);
+      break;
+  }
+}
+
+export default formatDay;
