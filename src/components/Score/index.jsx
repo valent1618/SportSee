ae1ebca35271880f1ae4899ec7ab009e732bc525
valent1618 <valentinlemaire1618@gmail.com>
2022-07-22 13:36:16 +0200

    add component score

--- src/components/Score/index.jsx
+++ src/components/Score/index.jsx
@@ -1,8 +1,36 @@
+import { RadialBarChart, RadialBar, ResponsiveContainer } from 'recharts';
+
 function Score({ score }) {
+  const data = [{ value: score, fill: '#FF0101' }];
+
+  // Push 7 object with value 1 in the data for fill the center of the RadialBarChart and define the max value
+  for (let i = 0; i < 8; i++) {
+    data.unshift({ value: 1, fill: 'white' });
+  }
+
   return (
     <div className='score'>
-      <h1>SCORE</h1>
-      <h2>{score}</h2>
+      <h5>Score</h5>
+      <div className='label'>
+        <h3>{score * 100}%</h3>
+        <h4>
+          de votre
+          <br />
+          objectif
+        </h4>
+      </div>
+      <ResponsiveContainer width='100%' height='100%'>
+        <RadialBarChart
+          margin={0}
+          barCategoryGap={0}
+          barGap={0}
+          innerRadius='0%'
+          outerRadius='100%'
+          data={data}
+        >
+          <RadialBar dataKey='value' cornerRadius={50} />
+        </RadialBarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
